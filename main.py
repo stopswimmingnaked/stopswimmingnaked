55a01731dec77851e1db1472869193b208b62913
stopswimmingnaked <hantonjulian@googlemail.com>
2025-04-03 10:29:51 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.title('stopswimmingnaked.com')
+st.title('stopswimmingnaked')
 st.markdown("""
 **A minimalist dashboard for deep company analysis.**
 """)
